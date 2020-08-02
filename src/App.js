47febd2ff20ec06f1cafebf0105e363d031312d6
nithin <mkrnithin@gmail.com>
2020-08-02 16:56:39 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from 'react';
+import './App.css';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Fragment>
+                    <p>This is an incredibly complex react app!</p>
+                </Fragment>
+            </div>
+        )
+    }
+}
+export default App;
